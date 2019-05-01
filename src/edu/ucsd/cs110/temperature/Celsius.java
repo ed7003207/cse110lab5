d04ac415efaa86c4673906e35acb8eb822573e40
Edward Yoon <e2yoon@ucsd.edu>
2019-05-01 12:42:07 -0700

    initial push

---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,26 @@
+package edu.ucsd.cs110.temperature;
+public class Celsius extends Temperature
+{
+    public Celsius(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return new Celsius(this.getValue());
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        float value = ((this.getValue() * 9/5)+32);
+        return new Celsius(value);
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        String cString = "" + this.getValue() + " C";
+        return cString;
+    }
+}
